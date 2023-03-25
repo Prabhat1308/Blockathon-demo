b454c367bfe9eb292b2a54f75811c5ff39f17df6
startup-dreamer <s_kbharbhunja@me.iitr.ac.in>
2023-03-26 00:28:06 +0530

    adding encryption module

--- encryption_module/test.js
+++ encryption_module/test.js
@@ -1,9 +1,8 @@
 import { uploadEncryptionIpfs } from "./index.js";
-require("dotenv").config({ path: "../../.env" });
-
+const WEB3_STORAGE_API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU5ODMzMDYwMjllNDQyNGM1MDg1NzUwNmM0MjY4MzhmOEUyMmUxMzIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Nzk3NTU2Mzc3NTAsIm5hbWUiOiJzdW1pdCJ9.hEzv-WkFKs6-nH0NoYKxACQFcOlg7mDNQqosVv2T2Nk"
 const run = async () => {
   const cid = await uploadEncryptionIpfs(
-    process.env.WEB3_STORAGE_API_KEY,
+    WEB3_STORAGE_API_KEY,
     "./hi",
     "password"
   );
--- encryption_module/utils/retrieveipfs.js
+++ encryption_module/utils/retrieveipfs.js
@@ -1,8 +1,7 @@
 import { Web3Storage } from "web3.storage";
 import fs from "fs";
 import path from "path";
-require("dotenv").config({ path: ".env" });
-const WEB3_STORAGE_API_KEY = process.env.WEB3_STORAGE_API_KEY;
+const WEB3_STORAGE_API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU5ODMzMDYwMjllNDQyNGM1MDg1NzUwNmM0MjY4MzhmOEUyMmUxMzIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Nzk3NTU2Mzc3NTAsIm5hbWUiOiJzdW1pdCJ9.hEzv-WkFKs6-nH0NoYKxACQFcOlg7mDNQqosVv2T2Nk"
 export async function downloadFile(
   WEB3_STORAGE_API_KEY,
   cid,
